8b42f4092c9c95a1215161b668a594b0c6acc932
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-06 20:46:15 +0300

    Add movie card imformation

--- movies-now/src/components/Movies/MovieCard.js
+++ movies-now/src/components/Movies/MovieCard.js
@@ -1,8 +1,19 @@
 const MovieCard = ({ movie }) => {
+    console.log(movie);
     return (
         <li
+            className="movie-card-element"
             id={movie.imdbID}>
-            {movie.Title}
+
+            <img src={
+                movie.Poster !== 'N/A'
+                    ? movie.Poster
+                    : 'https://miro.medium.com/max/1400/1*cLQUX8jM2bMdwMcV2yXWYA.jpeg'
+            } />
+
+            <h2 className="movie-card-title">{movie.Title}</h2>
+            <h3 className="movie-card-type">{movie.Type}</h3>
+            <h3 className="movie-card-year">{movie.Year}</h3>
         </li>
     )
 }
