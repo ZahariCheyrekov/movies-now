f8d88c3f5c48d60f691a9356e541bd61fa72b954
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-07 22:43:22 +0300

    Add header title element

--- movies-now/src/components/Home/Home.js
+++ movies-now/src/components/Home/Home.js
@@ -1,6 +1,12 @@
+import './Home.css';
+
 const Home = () => {
     return (
         <>
+            <h1 className="home-title">
+              
+                Explore your favorite movies, shows and series.
+            </h1>
         </>
     );
 }
