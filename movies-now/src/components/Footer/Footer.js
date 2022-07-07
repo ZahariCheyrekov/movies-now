98d90adab136869911d621350c7166fe4aeb0437
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-07 22:12:59 +0300

    Add footer component

---
+++ movies-now/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="site-footer">
+            <h2>
+                Movies Now 
+            </h2>
+        </footer>
+    );
+}
+
+export default Footer;
