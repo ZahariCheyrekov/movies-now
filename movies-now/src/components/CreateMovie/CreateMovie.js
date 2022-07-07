6e972a620d138cc4d7ad32d47bbd685be087bfc5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-07 18:55:24 +0300

    Add create movie label style

--- movies-now/src/components/CreateMovie/CreateMovie.js
+++ movies-now/src/components/CreateMovie/CreateMovie.js
@@ -13,21 +13,36 @@ const CreateMovie = () => {
         <form onSubmit={onSubmitHandler} className="create-movie-form" method="POST">
             <h2 className="create-movie-title">Create Movie</h2>
 
-            <label htmlFor="movie-title">Title:</label>
-            <input type="text" className="title-movie input-el-m" name="title" placeholder="Sonic the Hedgehog 2" />
+            <label htmlFor="movie-title" className="title-m-label">Title:
+                <input type="text" className="title-movie input-el-m" name="title"
+                    placeholder="Sonic the Hedgehog 2" />
+            </label>
 
-            <label htmlFor="movie-type">Type:</label>
-            <input type="text" className="type-movie input-el-m" name="type" placeholder="Action, Adventure, Comedy..." />
+            <label htmlFor="movie-type" className="type-m-label">Type:
+                <input type="text" className="type-movie input-el-m" name="type"
+                    placeholder="Action, Adventure, Comedy..." />
+            </label>
 
-            <label htmlFor="movie-year">Year:</label>
-            <input type="text" className="year-movie input-el-m" name="year" placeholder="2022" />
+            <label htmlFor="movie-year" className="year-m-label">Year:
+                <input type="text" className="year-movie input-el-m" name="year"
+                    placeholder="2022" />
+            </label>
 
-            <label htmlFor="movie-img">Image url:</label>
-            <input type="text" className="imageUrl-movie input-el-m" name="imageUrl" placeholder="https://dx35vtwkllhj9.cloudfront.net..." />
+            <label htmlFor="movie-img" className="image-m-label">Image url:
+                <input type="text" className="imageUrl-movie input-el-m" name="imageUrl"
+                    placeholder="https://dx35vtwkllhj9.cloudfront.net..." />
+            </label>
 
-            <label htmlFor="movie-description">Description:</label>
-            <textarea name="description" className="description-movie input-el-m" placeholder="Sonic the Hedgehog 2 is a 2022 action-adventure comedy film based on the video game series published by Sega..."></textarea>
-            <input className="btn submit" type="submit" value="Create Movie" />
+            <label htmlFor="movie-description" className="description-m-label">Description:
+                <textarea name="description" className="description-movie input-el-m"
+                    placeholder="Sonic the Hedgehog 2 is a 2022 action-adventure comedy film based on..."
+                ></textarea>
+            </label>
+
+            <label htmlFor="buttons">
+                <input className="btn submit" type="submit" value="Clear" />
+                <input className="btn submit" type="submit" value="Create Movie" />
+            </label>
         </form>
     );
 }
