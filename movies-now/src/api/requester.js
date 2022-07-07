c6029c8b9b5efaef587277c441cf05c5c6ee9923
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-07 16:20:12 +0300

    Add requester function implementation

---
+++ movies-now/src/api/requester.js
@@ -0,0 +1,30 @@
+export const requester = async (method, url, data) => {
+    let options = {
+        method,
+        headers: {}
+    }
+
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    try {
+        const res = await fetch(url, options);
+
+        if (res.ok == false) {
+            throw new Error(res.statusText);
+        }
+
+        const data = await res.json();
+        return data;
+
+    } catch (error) {
+        alert(error);
+    }
+}
+
+export const get = requester.bind(null, 'GET');
+export const put = requester.bind(null, 'PUT');
+export const post = requester.bind(null, 'POST');
+export const del = requester.bind(null, 'DELETE');
