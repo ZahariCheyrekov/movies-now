500028ef1a7c6144ecb27a0dfdff71b2f9d53801
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-07 18:28:58 +0300

    Edit create movie classes

--- movies-now/src/components/CreateMovie/CreateMovie.js
+++ movies-now/src/components/CreateMovie/CreateMovie.js
@@ -11,22 +11,23 @@ const CreateMovie = () => {
 
     return (
         <form onSubmit={onSubmitHandler} className="create-movie-form" method="POST">
-                <h2 className="create-movie-title">Create Movie</h2>
+            <h2 className="create-movie-title">Create Movie</h2>
 
-                <label htmlFor="movie-title">Title:</label>
-                <input type="text" className="title-movie" name="title" placeholder="Enter movie title" />
+            <label htmlFor="movie-title">Title:</label>
+            <input type="text" className="title-movie input-el-m" name="title" placeholder="Sonic the Hedgehog 2" />
 
-                <label htmlFor="movie-type">Type:</label>
-                <input type="text" className="type-movie" name="type" placeholder="Enter movie type" />
+            <label htmlFor="movie-type">Type:</label>
+            <input type="text" className="type-movie input-el-m" name="type" placeholder="Action, Adventure, Comedy..." />
 
-                <label htmlFor="movie-year">Year:</label>
-                <input type="text" className="year-movie" name="year" placeholder="2010" />
+            <label htmlFor="movie-year">Year:</label>
+            <input type="text" className="year-movie input-el-m" name="year" placeholder="2022" />
 
-                <label htmlFor="movie-img">Image url:</label>
-                <input type="text" className="imageUrl-movie" name="imageUrl" placeholder="Upload movie photo" />
+            <label htmlFor="movie-img">Image url:</label>
+            <input type="text" className="imageUrl-movie input-el-m" name="imageUrl" placeholder="https://dx35vtwkllhj9.cloudfront.net..." />
 
-                <textarea name="description" className="description-movie" placeholder="Enter movie description"></textarea>
-                <input className="btn submit" type="submit" value="Create Movie" />
+            <label htmlFor="movie-description">Description:</label>
+            <textarea name="description" className="description-movie input-el-m" placeholder="Sonic the Hedgehog 2 is a 2022 action-adventure comedy film based on the video game series published by Sega..."></textarea>
+            <input className="btn submit" type="submit" value="Create Movie" />
         </form>
     );
 }
