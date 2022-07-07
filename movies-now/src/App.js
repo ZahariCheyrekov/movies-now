eb74d0e7e6c337acc488a7a24113c694db4a79f0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-07 11:27:55 +0300

    Import header component

--- movies-now/src/App.js
+++ movies-now/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+
+import Header from './components/Header';
 import Movies from './components/Movies';
 
 function App() {
   return (
     <div className="site-content">
+      <Header />
       <Movies />
     </div>
   );
