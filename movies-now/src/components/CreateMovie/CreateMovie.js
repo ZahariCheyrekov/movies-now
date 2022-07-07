78127c13950615a2d1ab4db5261cfc950698792c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-07 17:58:12 +0300

    Edit create movie input fields class

--- movies-now/src/components/CreateMovie/CreateMovie.js
+++ movies-now/src/components/CreateMovie/CreateMovie.js
@@ -1,4 +1,5 @@
 import { createNewMovie } from '../../services/movieService';
+import './CreateMovie.css';
 
 const CreateMovie = () => {
     const onSubmitHandler = (ev) => {
@@ -9,25 +10,23 @@ const CreateMovie = () => {
     }
 
     return (
-        <form onSubmit={onSubmitHandler} id="create" method="POST">
-            <div className="container">
-                <h1>Create Game</h1>
+        <form onSubmit={onSubmitHandler} className="create-movie-form" method="POST">
+                <h2>Create Movie</h2>
 
-                <label htmlFor="movie-title">Movie title:</label>
-                <input type="text" id="title" name="title" placeholder="Enter movie title" />
+                <label htmlFor="movie-title">Title:</label>
+                <input type="text" className="title-movie" name="title" placeholder="Enter movie title" />
 
                 <label htmlFor="movie-type">Type:</label>
-                <input type="text" id="type" name="type" placeholder="Enter movie type" />
+                <input type="text" className="type-movie" name="type" placeholder="Enter movie type" />
 
                 <label htmlFor="movie-year">Year:</label>
-                <input type="text" id="year" name="year" placeholder="2010" />
+                <input type="text" className="year-movie" name="year" placeholder="2010" />
 
-                <label htmlFor="movie-img">Image:</label>
-                <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload movie photo" />
+                <label htmlFor="movie-img">Image url:</label>
+                <input type="text" className="imageUrl-movie" name="imageUrl" placeholder="Upload movie photo" />
 
-                <textarea name="description" id="description" placeholder="Enter movie description"></textarea>
+                <textarea name="description" className="description-movie" placeholder="Enter movie description"></textarea>
                 <input className="btn submit" type="submit" value="Create Movie" />
-            </div>
         </form>
     );
 }
