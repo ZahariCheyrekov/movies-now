f6ef8600285440258b2c311d2c8c300520dc0629
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-07 16:21:13 +0300

    Import create movie component

--- movies-now/src/App.js
+++ movies-now/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import Header from './components/Header';
+import CreateMovie from './components/CreateMovie';
 import Movies from './components/Movies';
 
 function App() {
   return (
     <div className="site-content">
       <Header />
+      <CreateMovie />
       <Movies />
     </div>
   );
