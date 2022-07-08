6dde51f4d4c8baa31ed6f5fbb05ac1e42528ebfa
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 14:35:37 +0300

    Add enjoy section content

--- movies-now/src/components/EnjoySection/EnjoySection.js
+++ movies-now/src/components/EnjoySection/EnjoySection.js
@@ -1,7 +1,23 @@
 const EnjoySection = () => {
     return (
+        <section className="explore-movies site-sec">
+            <div className="explr-mv-wrapper">
+                <article className="explr-mv-content">
+                    <h2 className="site-ttl-sec">
+                        Learn more about your favourite movies!
+                    </h2>
+                    <p className="site-p-sec">
+                        A place to find information about new movies, shows and series.
+                        Read about production process and actors experience.
+                    </p>
+                </article>
 
-);
+                <article className="explr-img-sec">
+                    <img src={sonicImg} />
+                </article>
+            </div>
+        </section>
+    );
 }
 
 export default EnjoySection;
