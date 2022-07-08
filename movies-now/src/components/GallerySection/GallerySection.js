160500e160526bcb163005f5645dc62710badef4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 16:54:00 +0300

    Import image assets

--- movies-now/src/components/GallerySection/GallerySection.js
+++ movies-now/src/components/GallerySection/GallerySection.js
@@ -1,17 +1,21 @@
+import shazamImg from '../../assets/shazam.jpg';
+import theMatrixImg from '../../assets/the-matrix.jpg';
+import f9Img from '../../assets/f9.jpg';
+
 const GallerySection = () => {
     return (
         <section className="explore-movies gallery-section" >
             <div className="explr-mv-wrapper gallery-sec-wrapper">
                 <article className="gallery-img-art">
-                    <img className="gallery-img" />
+                    <img src={shazamImg} className="gallery-img" alt="shazam" />
                 </article>
 
                 <article className="gallery-img-art">
-                    <img className="gallery-img" />
+                    <img src={theMatrixImg} className="gallery-img" alt="the-matrix" />
                 </article>
 
                 <article className="gallery-img-art">
-                    <img className="gallery-img" />
+                    <img src={f9Img} className="gallery-img" alt="fast-and-furious" />
                 </article>
             </div>
         </section>
