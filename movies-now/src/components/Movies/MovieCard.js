3fd7f9b349406a3a0de7eaf535bd2e121e2ee329
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 22:39:33 +0300

    Edit movie card content

--- movies-now/src/components/Movies/MovieCard.js
+++ movies-now/src/components/Movies/MovieCard.js
@@ -1,12 +1,12 @@
 import './MovieCard.css';
 import notFound from '../../assets/404.jpg';
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ id, movie }) => {
     console.log(movie);
     return (
         <li
             className="movie-card-element"
-            id={movie.imdbID}>
+            id={id}>
 
             <img src={
                 movie.Poster !== 'N/A'
@@ -14,10 +14,10 @@ const MovieCard = ({ movie }) => {
                     : notFound
             } alt="movie-poster" />
 
-            <h2 className="movie-card-title mv-el">{movie.Title}</h2>
+            <h2 className="movie-card-title mv-el">{movie.title}</h2>
             <hr className='movie-hr' />
-            <h3 className="movie-card-type mv-el">{movie.Type}</h3>
-            <h3 className="movie-card-year mv-el">{movie.Year}</h3>
+            <h3 className="movie-card-type mv-el">{movie.type}</h3>
+            <h3 className="movie-card-year mv-el">{movie.ear}</h3>
         </li>
     )
 }
