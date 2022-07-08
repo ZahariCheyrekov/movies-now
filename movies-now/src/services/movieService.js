535fb3f823d16f13d5e2bb8ee9ab46e6ec3a444e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 23:01:03 +0300

    Remove base url

--- movies-now/src/services/movieService.js
+++ movies-now/src/services/movieService.js
@@ -1,6 +1,5 @@
 import * as requester from '../api/requester.js';
 
-const baseUrl = 'http://www.omdbapi.com?apikey=5c5d0828&s=';
 const moviesUrl = 'https://chat-app-8cffe-default-rtdb.firebaseio.com/movies/.json';
 
 export const getAllMovies = () => requester.get(moviesUrl);
