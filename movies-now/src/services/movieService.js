bd5c64ad6c2e18dd7113754ee482627f86b7712d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 23:00:42 +0300

    Remove get movies by title function

--- movies-now/src/services/movieService.js
+++ movies-now/src/services/movieService.js
@@ -3,13 +3,6 @@ import * as requester from '../api/requester.js';
 const baseUrl = 'http://www.omdbapi.com?apikey=5c5d0828&s=';
 const moviesUrl = 'https://chat-app-8cffe-default-rtdb.firebaseio.com/movies/.json';
 
-export const getMoviesByTitle = async (title) => {
-    const res = await fetch(`${baseUrl}${title}`);
-    const data = await res.json();
-
-    return data.Search;
-}
-
 export const getAllMovies = () => requester.get(moviesUrl);
 
 export const createNewMovie = (data) => requester.post(moviesUrl, data);
