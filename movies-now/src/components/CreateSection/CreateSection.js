a20212be5c8612e506416601d02828549790a861
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 15:53:06 +0300

    Add create component content

--- movies-now/src/components/CreateSection/CreateSection.js
+++ movies-now/src/components/CreateSection/CreateSection.js
@@ -1,6 +1,22 @@
 const CreateSection = () => {
     return (
+        <section className="explore-movies site-sec">
+            <div className="explr-mv-wrapper">
+                <article className="explr-mv-content">
+                    <h2 className="site-ttl-sec">
+                        Create your own content.
+                    </h2>
+                    <p className="site-p-sec">
+                        Create your own unique content and start showing
+                        it to friends and other active users.
+                    </p>
+                </article>
 
+                <article className="explr-img-sec flm-flp-img">
+                    <img src={filmFlap} alt="film-flap" />
+                </article>
+            </div>
+        </section>
     );
 }
 
