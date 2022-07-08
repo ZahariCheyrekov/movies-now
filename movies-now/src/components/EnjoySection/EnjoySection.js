1fd8e60d7596d0c2f9f1aac8f61e5969341b91f0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 14:47:58 +0300

    Add enjoy section article

--- movies-now/src/components/EnjoySection/EnjoySection.js
+++ movies-now/src/components/EnjoySection/EnjoySection.js
@@ -1,19 +1,21 @@
+import peopleImg from '../../assets/people-talking.png';
+
 const EnjoySection = () => {
     return (
         <section className="explore-movies site-sec">
             <div className="explr-mv-wrapper">
-                <article className="explr-mv-content">
-                    <h2 className="site-ttl-sec">
-                        Learn more about your favourite movies!
-                    </h2>
-                    <p className="site-p-sec">
-                        A place to find information about new movies, shows and series.
-                        Read about production process and actors experience.
-                    </p>
+                <article className="explr-img-sec">
+                    <img src={peopleImg} />
                 </article>
 
-                <article className="explr-img-sec">
-                    <img src={sonicImg} />
+                <article className="explr-mv-content">
+                    <h2 className="site-ttl-sec">
+                        Remember to have fun.
+                    </h2>
+                    <p className="site-p-sec">
+                      They are plenty of movies in the collection where you can
+                      search for the ones you love.
+                    </p>
                 </article>
             </div>
         </section>
