dab8a678783b3df1a54e4e343e627058e9ade7d4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 12:34:47 +0300

    Edit explore section component

--- movies-now/src/components/ExploreSection/ExploreSection.js
+++ movies-now/src/components/ExploreSection/ExploreSection.js
@@ -1,7 +1,15 @@
 const ExploreSection = () => {
-return(
-    
-)
+    return (
+        <section className="explore-movies">
+            <div className="explr-mv-wrapper">
+                <article className="explr-mv-title">
+                    <h2 className="site-ttl-sec">
+                        Learn more about your favourite movies!
+                    </h2>
+                </article>
+            </div>
+        </section>
+    )
 }
 
 export default ExploreSection;
