af2bc049367f4a441e06ecd5965ee0ea32c86e1c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 16:47:37 +0300

    Add gallery component articles

--- movies-now/src/components/GallerySection/GallerySection.js
+++ movies-now/src/components/GallerySection/GallerySection.js
@@ -1,6 +1,20 @@
 const GallerySection = () => {
     return (
+        <section className="explore-movies gallery-section" >
+            <div className="explr-mv-wrapper gallery-sec-wrapper">
+                <article className="gallery-img-art">
+                    <img className="gallery-img" />
+                </article>
 
+                <article className="gallery-img-art">
+                    <img className="gallery-img" />
+                </article>
+
+                <article className="gallery-img-art">
+                    <img className="gallery-img" />
+                </article>
+            </div>
+        </section>
     );
 }
 
