f9e1e040102cc55ce2e88591032486716d129f2d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-08 12:46:30 +0300

    Add sonic img asset

--- movies-now/src/components/ExploreSection/ExploreSection.js
+++ movies-now/src/components/ExploreSection/ExploreSection.js
@@ -6,6 +6,14 @@ const ExploreSection = () => {
                     <h2 className="site-ttl-sec">
                         Learn more about your favourite movies!
                     </h2>
+                    <p className="site-p-sec">
+                        A place to find information about new movies, shows and series.
+                        Read about production process and actors experience.
+                    </p>
+                </article>
+
+                <article className="explr-img-sec">
+                    <img />
                 </article>
             </div>
         </section>
