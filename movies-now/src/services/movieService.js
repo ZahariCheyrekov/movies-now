a137b0e7ede7de5d821652f8b38fb2bc0cbcb2e1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-09 09:00:46 +0300

    Add get movie card by id function

--- movies-now/src/services/movieService.js
+++ movies-now/src/services/movieService.js
@@ -1,7 +1,10 @@
 import * as requester from '../api/requester.js';
 
-const moviesUrl = 'https://chat-app-8cffe-default-rtdb.firebaseio.com/movies/.json';
 
-export const getAllMovies = () => requester.get(moviesUrl);
+const moviesUrl = 'https://chat-app-8cffe-default-rtdb.firebaseio.com/movies';
 
-export const createNewMovie = (data) => requester.post(moviesUrl, data);
+export const getAllMovies = () => requester.get(`${moviesUrl}.json`);
+
+export const getMovieCardById = (movieId) => requester.get(`${moviesUrl}/${movieId}.json`);
+
+export const createNewMovie = (data) => requester.post(`${moviesUrl}.json`, data);
