d28f53b3e5443c0178af13943d7da5ded2b4c47f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-09 11:23:22 +0300

    Import header poster component

--- movies-now/src/components/Home/Home.js
+++ movies-now/src/components/Home/Home.js
@@ -4,10 +4,13 @@ import ExploreSection from '../ExploreSection';
 import EnjoySection from '../EnjoySection';
 import CreateSection from '../CreateSection';
 import GallerySection from '../GallerySection';
+import HeaderPoster from '../HeaderPoster/HeaderPoster';
 
 const Home = () => {
     return (
         <>
+            <HeaderPoster />
+
             <h1 className="home-title">
                 Explore your favorite movies, shows and series.
             </h1>
