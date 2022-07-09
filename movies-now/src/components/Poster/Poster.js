a91ce6445d3fa5f080615f4d36b1823fbd4ba9d9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-09 11:46:28 +0300

    Add poster component

---
+++ movies-now/src/components/Poster/Poster.js
@@ -0,0 +1,13 @@
+import './Poster.css';
+
+import moviesCollage from '../../assets/movies-collage.jpg'
+
+const HeaderPoster = () => {
+    return (
+        <article className="mv-collage-img-art">
+            <img src={moviesCollage} alt="movie-collage" />
+        </article>
+    )
+}
+
+export default HeaderPoster;
