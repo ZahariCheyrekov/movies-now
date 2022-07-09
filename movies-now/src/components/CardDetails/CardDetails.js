6d2d6852bf937358ddb2a1b16cc1b4b7132bb5fc
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-09 12:56:47 +0300

    Add card details component info

--- movies-now/src/components/CardDetails/CardDetails.js
+++ movies-now/src/components/CardDetails/CardDetails.js
@@ -17,6 +17,26 @@ const CardDetails = () => {
 
     return (
         <article className="movie-card-details">
+            <article className="mv-card-img-d">
+                <img src={movie.imageUrl} />
+            </article>
+
+            <aside className="mv-card-details-info">
+                <h2 className="mv-card-ttl">
+                    {movie.title}
+                </h2>
+                <h3 className="mv-card-type">
+                    {movie.type}
+                </h3>
+
+                <p className="mv-card-description">
+                    {movie.description}
+                </p>
+
+                <h4 className="mv-card-year">
+                    {movie.year}
+                </h4>
+            </aside>
         </article>
     );
 }
