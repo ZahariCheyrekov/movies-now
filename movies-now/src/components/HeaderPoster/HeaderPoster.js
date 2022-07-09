91ee4af92fd19e384153a65881c4548363e93710
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-09 11:22:22 +0300

    Add header poster content

--- movies-now/src/components/HeaderPoster/HeaderPoster.js
+++ movies-now/src/components/HeaderPoster/HeaderPoster.js
@@ -1,7 +1,12 @@
+
+import moviesCollage from '../../assets/movies-collage.jpg'
+
 const HeaderPoster = () => {
-return(
-    
-)
+    return (
+        <article className="mv-collage-img-art">
+            <img src={moviesCollage} alt="movie-collage" />
+        </article>
+    )
 }
 
 export default HeaderPoster;
