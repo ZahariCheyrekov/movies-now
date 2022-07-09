1d5cfe826c20b1513c61c20acffef8994e93d70e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-09 20:36:29 +0300

    Add get movies by genre function logic

--- movies-now/src/components/Movies/Movies.js
+++ movies-now/src/components/Movies/Movies.js
@@ -17,16 +17,67 @@ const Movies = () => {
         setMovies(Object.entries(moviesDb));
     }
 
+    const getMoviesByGenre = async (genre) => {
+        const moviesDb = await getAllMovies();
+
+        const moviesByCategory = [];
+        Object.entries(moviesDb).forEach(movie => {
+            const movieData = Object.values(movie[1]);
+
+            for (const value of movieData) {
+                if (typeof value === 'string') {
+                    if (value.includes(genre)) {
+                        moviesByCategory.push(movie);
+                    }
+                }
+            }
+        });
+
+        setMovies(Object.values(moviesByCategory))
+    }
+
     return (
         <>
             <ul className="mv-genres-ul">
-                <li className="mv-li-item actv">Action</li>
-                <li className="mv-li-item">Adventure</li>
-                <li className="mv-li-item">Animation</li>
-                <li className="mv-li-item">Comedy</li>
-                <li className="mv-li-item">Fantasy</li>
-                <li className="mv-li-item">Horror</li>
+                <li
+                    key={'All'}
+                    onClick={() => getMovies()}
+                    className="mv-li-item actv">
+                    All
+                </li>
+                <li
+                    key={'Action'}
+                    onClick={() => getMoviesByGenre('Action')}
+                    className="mv-li-item">
+                    Action
+                </li>
+                <li
+                    key={'Adventure'}
+                    onClick={() => getMoviesByGenre('Adventure')}
+                    className="mv-li-item">
+                    Adventure</li>
+                <li
+                    key={'Animation'}
+                    onClick={() => getMoviesByGenre('Animation')}
+                    className="mv-li-item">
+                    Animation</li>
+                <li
+                    key={'Comedy'}
+                    onClick={() => getMoviesByGenre('Comedy')}
+                    className="mv-li-item">
+                    Comedy</li>
+                <li
+                    key={'Fantasy'}
+                    onClick={() => getMoviesByGenre('Fantasy')}
+                    className="mv-li-item">
+                    Fantasy</li>
+                <li
+                    key={'Horror'}
+                    onClick={() => getMoviesByGenre('Horror')}
+                    className="mv-li-item">
+                    Horror</li>
             </ul>
+
             <ul className='movies-list'>
                 {movies
                     ? movies.map(movie =>
