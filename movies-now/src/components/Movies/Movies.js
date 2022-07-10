0687bf19c4e050e6dd78e1e01b19115c77e153f8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-10 17:07:47 +0300

    Edit movies component

--- movies-now/src/components/Movies/Movies.js
+++ movies-now/src/components/Movies/Movies.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './Movies.css';
-import MovieCard from '../MovieCard';
 
 import { useState, useEffect } from 'react';
-import useGenre from '../../hooks/useGenre';
 
 import { getAllMovies } from '../../services/movieService';
 
-import { toggleActiveStyle } from '../../utils/genreUtil';
-import { getMoviesByGenre } from '../../utils/movieUtil';
 import MovieList from './MovieList/MovieList';
+import MovieGenres from './MovieGenres/MovieGenres';
 
 const Movies = () => {
-    const genres = useGenre();
     const [movies, setMovies] = useState([]);
     const [isActive, setIsActive] = useState(false);
     const [selected, setSelected] = useState('All');
@@ -29,36 +25,21 @@ const Movies = () => {
     return (
         <>
             <div className="container">
-                <div className="dropdown">
-                    <section className="genre-options"
-                        onClick={() => setIsActive(!isActive)}
-                    >
-                        {selected}
-                        <i className="fa-solid fa-caret-down"></i>
+                <section className="genre-options"
+                    onClick={() => setIsActive(!isActive)}
+                >
+                    {selected}
+                    <i className="fa-solid fa-caret-down"></i>
 
-                        {isActive &&
-                            <ul className="mv-genres-ul">
-                                {genres.map(genre => (
-                                    <li
-                                        key={genre}
-                                        className={toggleActiveStyle(selected, genre)}
-                                        onClick={async () => {
-                                            setSelected(genre);
+                    <MovieGenres
+                        selected={selected}
+                        setSelected={setSelected}
+                        setMovies={setMovies}
+                        isActive={isActive}
+                        getMovies={getMovies}
+                    />
 
-                                            if (genre === 'All') {
-                                                getMovies();
-                                            } else {
-                                                const movies = await getMoviesByGenre(genre);
-                                                setMovies(movies);
-                                            }
-                                        }}
-                                    >{genre}
-                                    </li>
-                                ))}
-                            </ul>
-                        }
-                    </section>
-                </div>
+                </section>
             </div>
 
             <MovieList movies={movies} />
