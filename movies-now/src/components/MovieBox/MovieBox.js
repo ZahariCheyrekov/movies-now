198947077cf2b3a88d528b01aa2b4d1325b22390
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-10 18:18:04 +0300

    Import movie box css

--- movies-now/src/components/MovieBox/MovieBox.js
+++ movies-now/src/components/MovieBox/MovieBox.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './MovieBox.css';
+
 const MovieBox = () => {
     return (
         <section className="movie-box-sec">
-            
+
         </section>
     );
 }
