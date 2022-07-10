856962ca5696b68dba96926cb2be8ab2d6264d80
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-10 13:45:31 +0300

    Edit movie ids

--- movies-now/src/components/Movies/Movies.js
+++ movies-now/src/components/Movies/Movies.js
@@ -15,7 +15,7 @@ const Movies = () => {
     const [appState, changeState] = useState({
         activeObject: null,
         objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }, { id: 9 }
-            , { id: 10 }, { id: 11 }, { id: 12 }, { id: 13 }, { id: 14 }, { id: 15 }]
+            , { id: 10 }, { id: 11 }, { id: 12 }, { id: 13 }, { id: 14 }, { id: 15 }, { id: 16 }]
     });
 
     useEffect(() => {
