e7a4b3427dbe5045a3f7f6759ab6de2496fd0300
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-10 19:22:29 +0300

    Add movie box link

--- movies-now/src/components/MovieBox/MovieBox.js
+++ movies-now/src/components/MovieBox/MovieBox.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './MovieBox.css';
 
 const MovieBox = () => {
     return (
         <section className="movie-box-sec">
-            <article className="banner-art-container">
-                <h2 className="banner-art-title">
-                    Sonic the Hedgehog 2
-                </h2>
+            <Link to="details/-N6YXgeVKKq_q_aF20Cr">
+                <article className="banner-art-container">
+                    <h2 className="banner-art-title">
+                        Sonic the Hedgehog 2
+                    </h2>
 
-                <p className="banner-art-text">
-                    Sonic is rescued by Miles "Tails" Prower, a two-tailed fox who idolizes him
-                    and comes to warn him about Knuckles. Sonic convinces Tails to help him find
-                    the Master Emerald, while Robotnik reunites with his assistant Stone and,
-                    scheming to steal the emerald, offers to help Knuckles.
-                </p>
-            </article>
+                    <p className="banner-art-text">
+                        Sonic convinces Tails to help him find
+                        the Master Emerald, while Robotnik reunites with his assistant Stone and,
+                        scheming to steal the emerald, offers to help Knuckles.
+                    </p>
+                </article>
+            </Link>
         </section>
     );
 }
