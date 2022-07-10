ebd2c54fa0cc7327b4d76eca724359f4237b662f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-10 18:15:59 +0300

    Edit movie box component

--- movies-now/src/components/MovieBox/MovieBox.js
+++ movies-now/src/components/MovieBox/MovieBox.js
@@ -1,7 +1,11 @@
+import React from 'react';
+
 const MovieBox = () => {
     return (
-        
-);
+        <section className="movie-box-sec">
+            
+        </section>
+    );
 }
 
 export default MovieBox;
