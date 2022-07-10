7928118a24de892a768b5a39087c95582c83fa13
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-10 19:07:15 +0300

    Edit movie box style

--- movies-now/src/components/MovieBox/MovieBox.js
+++ movies-now/src/components/MovieBox/MovieBox.js
@@ -6,7 +6,16 @@ const MovieBox = () => {
     return (
         <section className="movie-box-sec">
             <article className="banner-art-container">
+                <h2 className="banner-art-title">
+                    Sonic the Hedgehog 2
+                </h2>
 
+                <p className="banner-art-text">
+                    Sonic is rescued by Miles "Tails" Prower, a two-tailed fox who idolizes him
+                    and comes to warn him about Knuckles. Sonic convinces Tails to help him find
+                    the Master Emerald, while Robotnik reunites with his assistant Stone and,
+                    scheming to steal the emerald, offers to help Knuckles.
+                </p>
             </article>
         </section>
     );
