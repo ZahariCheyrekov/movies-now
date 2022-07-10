380d9fe16728c0bf7c2dbdc1a30fc91a125ca66a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-10 13:40:19 +0300

    Edit appState object ids

--- movies-now/src/components/Movies/Movies.js
+++ movies-now/src/components/Movies/Movies.js
@@ -14,7 +14,8 @@ const Movies = () => {
     const [selected, setSelected] = useState('All');
     const [appState, changeState] = useState({
         activeObject: null,
-        objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }, { id: 9 }, { id: 10 }, { id: 11 }, { id: 12 }, { id: 13 }, { id: 14 }]
+        objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }, { id: 9 }
+            , { id: 10 }, { id: 11 }, { id: 12 }, { id: 13 }, { id: 14 }, { id: 15 }]
     });
 
     useEffect(() => {
