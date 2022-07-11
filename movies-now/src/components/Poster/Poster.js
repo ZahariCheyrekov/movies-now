0120e4d0336907d0e84b0cd7c646a2cd1ab017cf
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-11 16:33:15 +0300

    Edit header poster component

--- movies-now/src/components/Poster/Poster.js
+++ movies-now/src/components/Poster/Poster.js
@@ -1,8 +1,6 @@
 import './Poster.css';
 import React from 'react';
 
-import moviesCollage from '../../assets/movies-collage.jpg'
-
 const HeaderPoster = () => {
     return (
         <article className="mv-collage-img-art"></article>
