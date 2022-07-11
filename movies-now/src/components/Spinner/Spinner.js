1dc941f0761c9c736cddfaebcc17cde6ef7a6c4b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-11 17:45:46 +0300

    Edit spinner component

--- movies-now/src/components/Spinner/Spinner.js
+++ movies-now/src/components/Spinner/Spinner.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactLoading from 'react-loading';
 
 const Spinner = () => {
+    const [styles, setStyles] = useState({});
+
+    useEffect(() => {
+        setStyles({
+            margin: "0 auto",
+            display: "flex",
+            width: "100px",
+            height: "80vh",
+            fill: "red"
+        });
+    }, []);
+
     return (
         < ReactLoading
-            type="spin" color="#0000FF"
-            height={'100vh'} width={100}
+            type="spin"
+            style={styles}
         />
     );
 }
