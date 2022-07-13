a77715b68989618cb948a8c2423f159fce0224f1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-13 17:51:21 +0300

    Edit user service

--- movies-now/src/services/userService.js
+++ movies-now/src/services/userService.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../../src/firebase-config';
 
 export const login = async (loginEmail, loginPassword) => {
