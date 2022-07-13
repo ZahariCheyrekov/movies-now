d7000829f88cea7c0fd4f7f5f33e2726da1c2b9c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-13 15:45:34 +0300

    Edit header component

--- movies-now/src/components/Header/Header.js
+++ movies-now/src/components/Header/Header.js
@@ -1,11 +1,22 @@
-import './Header.css';
-import React from 'react';
-
-import popcornBag from '../../assets/popcorn-bag.png';
-
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import './Header.css';
+
+import popcornBag from '../../assets/popcorn-bag.png';
+import { getUser } from '../../services/userService';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../firebase-config';
+
+
 const Header = () => {
+    const [user, setUser] = useState({});
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+        console.log(currentUser)
+    });
+
     return (
         <header className="site-header">
             <div className='header-wrapper'>
@@ -22,13 +33,33 @@ const Header = () => {
                 <nav className="header-nav">
                     <Link to="/">Home</Link>
                     <Link to="/movies">Movies</Link>
-                    <Link to="/create">Create Movie</Link>
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
+
+                    {user
+                        ? userLinks()
+                        : guestLinks()
+                    }
                 </nav>
             </div>
         </header>
     );
+}
+
+const userLinks = () => {
+    return (
+        <>
+            <Link to="/create">Create Movie</Link>
+            <Link to="/logout">Logout</Link>
+        </>
+    )
+}
+
+const guestLinks = () => {
+    return (
+        <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+        </>
+    )
 }
 
 export default Header;
