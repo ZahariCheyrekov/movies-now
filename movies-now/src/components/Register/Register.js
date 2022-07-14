a7d3c3a1bfe1d08f51ab1f9b8d6fe9d04d9deb45
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-14 09:38:20 +0300

    Edit register component

--- movies-now/src/components/Register/Register.js
+++ movies-now/src/components/Register/Register.js
@@ -10,52 +10,49 @@ const Register = () => {
     const [repeatPassword, setRepeatPassword] = useState('');
 
     return (
+        <form className="input-form" action="register-user">
+            <h1 className="register-form-ttl frm-el">Register</h1>
+            <p className="register-form-create-accnt frm-el">Please fill in this form to create an account.</p>
+            <hr />
 
-        <form action="action_page.php">
-            <div className="container">
-                <h1>Register</h1>
-                <p>Please fill in this form to create an account.</p>
-                <hr />
+            <label htmlFor="email" className="frm-el"><b>Email</b></label>
+            <input
+                type="text"
+                placeholder="Email"
+                name="email"
+                id="email"
+                className="input-el frm-el"
+                required
+                onChange={(ev) => setRegisterEmail(ev.target.value)}
+            />
 
-                <label htmlFor="email"><b>Email</b></label>
-                <input
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                    id="email"
-                    required
-                    onChange={(ev) => setRegisterEmail(ev.target.value)}
-                />
+            <label htmlFor="psw" className="frm-el"><b>Password</b></label>
+            <input
+                type="password"
+                placeholder="Password"
+                name="psw"
+                id="psw"
+                className="input-el frm-el"
+                required
+                onChange={(ev) => setRegisterPassword(ev.target.value)}
+            />
 
-                <label htmlFor="psw"><b>Password</b></label>
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="psw"
-                    id="psw"
-                    required
-                    onChange={(ev) => setRegisterPassword(ev.target.value)}
-                />
+            <label htmlFor="psw-repeat frm-el"><b>Repeat Password</b></label>
+            <input
+                type="password"
+                placeholder="Repeat Password"
+                name="psw-repeat"
+                id="psw-repeat"
+                className="input-el frm-el"
+                required
+                onChange={(ev) => setRepeatPassword(ev.target.value)} />
+            <hr />
 
-                <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-                <input
-                    type="password"
-                    placeholder="Repeat Password"
-                    name="psw-repeat"
-                    id="psw-repeat"
-                    required
-                    onChange={(ev) => setRepeatPassword(ev.target.value)} />
-                <hr />
+            <button
+                onClick={() => register(registerEmail, registerPassword, repeatPassword)}
+                className="registerbtn frm-el">Register</button>
 
-                <p>By creating an account you agree to our <Link to="#">Terms  Privacy</Link>.</p>
-                <button
-                    onClick={() => register(registerEmail, registerPassword, repeatPassword)}
-                    type="button" className="registerbtn">Register</button>
-            </div>
-
-            <div className="container signin">
-                <p>Already have an account? <Link to="/login">Sign in</Link>.</p>
-            </div>
+            <p>Already have an account? <Link to="/login">Sign in</Link>.</p>
         </form>
 
     );
