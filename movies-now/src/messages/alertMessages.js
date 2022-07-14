7049b7f1887e0fb5bd88ea4b921a6addc37ead47
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-14 09:20:45 +0300

    Add invalid password length message

--- movies-now/src/messages/alertMessages.js
+++ movies-now/src/messages/alertMessages.js
@@ -1,3 +1,5 @@
 export const ALL_FIELDS_ARE_REQUIRED_MESSAGE = 'All fields are required!';
 
 export const PASSWORDS_MUST_MATCH_MESSAGE = 'Passwords must match!';
+
+export const INVALID_PASSWORD_LENGTH_MESSAGE = 'Password must be at least 6 characters long!';
