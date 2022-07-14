19e8cc4f07e0b56c566717e25c5f9a79feed0353
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-14 09:38:06 +0300

    Edit input validator

--- movies-now/src/validators/inputValidator.js
+++ movies-now/src/validators/inputValidator.js
@@ -1,5 +1,9 @@
+import { ALL_FIELDS_ARE_REQUIRED_MESSAGE } from "../messages/alertMessages";
+
 export const inputValidator = (data) => {
+    console.log(data);
     if (Object.values(data).some(x => x.trim() === '')) {
+        alert(ALL_FIELDS_ARE_REQUIRED_MESSAGE);
         return false;
     }
 
