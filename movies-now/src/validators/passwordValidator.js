d7d19300906dde8867128330ae9be55b2afae31b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-14 09:19:51 +0300

    Edit password validator

--- movies-now/src/validators/passwordValidator.js
+++ movies-now/src/validators/passwordValidator.js
@@ -1,3 +1,15 @@
+import { INVALID_PASSWORD_LENGTH_MESSAGE, PASSWORDS_MUST_MATCH_MESSAGE } from '../messages/alertMessages';
+
 export const passwordValidator = (pass, repass) => {
-    return pass.trim() === repass.trim();
+    if (pass.length < 6) {
+        alert(INVALID_PASSWORD_LENGTH_MESSAGE);
+        return false;
+    }
+
+    if (pass !== repass) {
+        alert(PASSWORDS_MUST_MATCH_MESSAGE);
+        return false;
+    }
+
+    return true;
 }
