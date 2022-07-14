f0310ea8c851abec0bc2fc70ff737500340b9c0e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-14 10:08:04 +0300

    Edit login component

--- movies-now/src/components/Login/Login.js
+++ movies-now/src/components/Login/Login.js
@@ -1,27 +1,55 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { login } from '../../services/userService';
 
-const Login = ({ setUser }) => {
+const Login = () => {
     const [loginEmail, setLoginEmail] = useState('');
     const [loginPassword, setLoginPassword] = useState('');
 
     return (
-        <>
-            <div >
-                <h3>Login</h3>
-                <input
-                    placeholder="Email..."
-                    onChange={(ev) => setLoginEmail(ev.target.value)}
-                />
-                <input
-                    placeholder="Password..."
-                    onChange={(ev) => setLoginPassword(ev.target.value)}
-                />
+        <form className="input-form" action="login-user">
+            <h1 className="login-form-ttl frm-el">
+                Login
+            </h1>
 
-                <button onClick={() => login(loginEmail, loginPassword)}>Login</button>
-            </div>
-        </>
+            <label htmlFor="email" className="frm-el"><b>Email</b></label>
+            <input
+                type="text"
+                placeholder="Email"
+                name="email"
+                id="email"
+                className="input-el frm-el"
+                required
+                onChange={(ev) => setLoginEmail(ev.target.value)}
+            />
+
+            <label htmlFor="psw" className="frm-el">
+                <strong>
+                    Password
+                </strong>
+            </label>
+            <input
+                type="password"
+                placeholder="Password"
+                name="psw"
+                id="psw"
+                className="input-el frm-el"
+                required
+                onChange={(ev) => setLoginPassword(ev.target.value)}
+            />
+
+            <button
+                onClick={() => login(loginEmail, loginPassword)}
+                className="loginbtn frm-el red-btn">Login</button>
+
+            <p className="alrd-have-acc-txt">
+                Dont't have an account?
+                <Link className="frm-link" to="/register">
+                    Register
+                </Link>
+            </p>
+        </form>
     )
 }
 
