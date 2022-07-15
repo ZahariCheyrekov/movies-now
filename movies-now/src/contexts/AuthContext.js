425245a3ce9ab3154d0134d01a8d0ccd22696a48
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-15 10:07:39 +0300

    Add auth context

---
+++ movies-now/src/contexts/AuthContext.js
@@ -0,0 +1,23 @@
+import { onAuthStateChanged } from 'firebase/auth';
+import { createContext, useEffect, useState } from 'react';
+import { auth } from '../firebase-config';
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            setCurrentUser(currentUser);
+        });
+    }, []);
+
+    return (
+        <AuthContext.Provider
+            value={currentUser}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+}
