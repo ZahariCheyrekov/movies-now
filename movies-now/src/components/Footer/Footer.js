184e4dabc47bba596717f6942887f9d176504866
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-16 12:02:07 +0300

    Edit footer style

--- movies-now/src/components/Footer/Footer.js
+++ movies-now/src/components/Footer/Footer.js
@@ -8,6 +8,44 @@ const Footer = () => {
                 <h2 className="footer-title mv-now">
                     Movies Now
                 </h2>
+
+                <ul className="footer-list">
+                    <ul className="first-list">
+                        <li className="footer-li-item">
+                            FAQ
+                        </li>
+                        <li className="footer-li-item">
+                            Privacy
+                        </li>
+                        <li className="footer-li-item">
+                            Help Center
+                        </li>
+                    </ul>
+
+                    <ul className="middle-list">
+                        <li className="footer-li-item">
+                            Account
+                        </li>
+                        <li className="footer-li-item">
+                            Terms of Use
+                        </li>
+                        <li className="footer-li-item">
+                            Contact Us
+                        </li>
+                    </ul>
+
+                    <ul className="last-list">
+                        <li className="footer-li-item">
+                            Media Center
+                        </li>
+                        <li className="footer-li-item">
+                            Movies Place
+                        </li>
+                        <li className="footer-li-item">
+                            Media Center
+                        </li>
+                    </ul>
+                </ul>
             </div>
         </footer>
     );
