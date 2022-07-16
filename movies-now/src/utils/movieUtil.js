bb23523739b9de028cacb5acbd126b6b57f4f44d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-16 10:26:26 +0300

    Edit get movies by data function

--- movies-now/src/utils/movieUtil.js
+++ movies-now/src/utils/movieUtil.js
@@ -1,30 +1,24 @@
 import { getAllMovies } from '../services/movieService';
 
-// TODO: Refactor!!!
-
-export const getMoviesByGenre = async (genre) => {
-    const moviesDb = await getAllMovies();
-
-    const moviesByCategory = [];
-    Object.entries(moviesDb).forEach(movie => {
-        const movieGenres = Object.values(movie[1])[5];
-
-        if (movieGenres.toUpperCase().includes(genre.toUpperCase())) {
-            moviesByCategory.push(movie);
-        }
-    });
-
-    return Object.values(moviesByCategory);
+const movieData = {
+    description: 0,
+    imageUrl: 1,
+    likes: 2,
+    title: 3,
+    trailerUrl: 4,
+    type: 5,
+    year: 6,
 }
 
-export const getMoviesByTitle = async (title) => {
+export const getMoviesByData = async (data, movieDataType) => {
     const moviesDb = await getAllMovies();
 
     const moviesByTitle = [];
     Object.entries(moviesDb).forEach(movie => {
-        const movieTitle = Object.values(movie[1])[3];
+        const movieDataNumber = movieData[movieDataType];
+        const movieTitle = Object.values(movie[1])[movieDataNumber];
 
-        if (movieTitle.toUpperCase().includes(title.toUpperCase())) {
+        if (movieTitle.toUpperCase().includes(data.toUpperCase())) {
             moviesByTitle.push(movie);
         }
     });
