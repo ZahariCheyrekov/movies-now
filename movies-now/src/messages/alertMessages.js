7386b8e40209c352ff2e68708f7f877fb9cee196
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-17 18:53:02 +0300

    Add invalid movie year alert message

--- movies-now/src/messages/alertMessages.js
+++ movies-now/src/messages/alertMessages.js
@@ -3,3 +3,5 @@ export const ALL_FIELDS_ARE_REQUIRED_MESSAGE = 'All fields are required!';
 export const PASSWORDS_MUST_MATCH_MESSAGE = 'Passwords must match!';
 
 export const INVALID_PASSWORD_LENGTH_MESSAGE = 'Password must be at least 6 characters long!';
+
+export const INVALID_MOVIE_YEAR_INPUT_MESSAGE = 'The year must be between 1888 and current year!';
