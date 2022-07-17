98f0f42379c05dbb409e09f16d34e1ec4982cdeb
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-17 16:19:04 +0300

    Edit create movie handler function

--- movies-now/src/handlers/createMovieHandler.js
+++ movies-now/src/handlers/createMovieHandler.js
@@ -2,18 +2,14 @@ import { createNewMovie } from '../services/movieService';
 import { inputValidator } from '../validators/inputValidator';
 
 export const createMovieHandler = (ev, navigate) => {
-    console.log();
     ev.preventDefault();
 
     const data = Object.fromEntries(new FormData(ev.currentTarget.parentNode.parentNode));
-
     const isValid = inputValidator(data);
 
     if (!isValid) {
         return;
     }
-
-    data.likes = 0;
 
     createNewMovie(data);
     navigate('/movies')
